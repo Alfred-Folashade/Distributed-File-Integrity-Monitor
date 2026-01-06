0d4b5528f814624b1188c7585cdea61fc7dcecaa
Alfred-Folashade <129620082+Alfred-Folashade@users.noreply.github.com>
2026-01-06 17:18:46 +0000

    Add replicate data function to modularise code

--- FileIntegrityMonitoring/Main.java
+++ FileIntegrityMonitoring/Main.java
@@ -8,28 +8,36 @@ import java.nio.file.StandardCopyOption;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		
+
+
+		File data = new File("data.txt");
+		data.createNewFile();
+		
+		
+		replicateData(data);
+		// clean up	
+		// Files.delete(Paths.get("StorageNodeA/data.txt"));
+		// Files.delete(Paths.get("StorageNodeB/data.txt"));
+		// Files.delete(Paths.get("StorageNodeC/data.txt")); 
+				
+		// Files.delete(Paths.get("StorageNodeA"));
+		// Files.delete(Paths.get("StorageNodeB"));
+		// Files.delete(Paths.get("StorageNodeC")); 
+				
+		
+		
+		
+	}
+	
+	public static void replicateData(File data) throws IOException {
 		Files.createDirectories(Paths.get("StorageNodeA"));
 		Files.createDirectories(Paths.get("StorageNodeB"));
 		Files.createDirectories(Paths.get("StorageNodeC"));
-
-
-		File data = new File("StorageNodeA/data.txt");
-		data.createNewFile();
-		Files.copy(Paths.get("StorageNodeA/data.txt"), Paths.get("StorageNodeB").resolve("data.txt"));
-		Files.copy(Paths.get("StorageNodeA/data.txt"), Paths.get("StorageNodeC/data.txt"));
 		
-		// clean up	
-		Files.delete(Paths.get("StorageNodeA/data.txt"));
-		Files.delete(Paths.get("StorageNodeB/data.txt"));
-		Files.delete(Paths.get("StorageNodeC/data.txt")); 
-				
-		Files.delete(Paths.get("StorageNodeA"));
-		Files.delete(Paths.get("StorageNodeB"));
-		Files.delete(Paths.get("StorageNodeC")); 
-				
-		
-		
-		
+		Files.copy(Paths.get(data.getAbsolutePath()), Paths.get("StorageNodeA").resolve(data.getName()));
+		Files.copy(Paths.get(data.getAbsolutePath()), Paths.get("StorageNodeB").resolve(data.getName()));
+		Files.copy(Paths.get(data.getAbsolutePath()), Paths.get("StorageNodeC").resolve(data.getName()));
 	}
 
 }
